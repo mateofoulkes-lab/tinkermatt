c125b1fb8de9bfb078670ae0a3795e78364f11d5
mateofoulkes-lab <mateofoulkes@gmail.com>
2026-09-18 12:40:12 -0300

    Load v0.4.1 refinement layer after v0.4

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ function tinkermattV04Bridge(): Plugin {
       const normalized = id.replace(/\\/g, "/");
       if (!normalized.endsWith("/src/main.ts")) return null;
       return {
-        code: `${code}\nObject.assign(window, { __tinkerEditor: editor });\nimport(\"./v04\");\n`,
+        code: `${code}\nObject.assign(window, { __tinkerEditor: editor });\nimport(\"./v04\").then(() => import(\"./v041\"));\n`,
         map: null,
       };
     },
