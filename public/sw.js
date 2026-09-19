f7a1eec1f6c64e8a685c375b725bc2964e3e8b3a
mateofoulkes-lab <mateofoulkes@gmail.com>
2026-09-19 10:28:38 -0300

    Invalidate cache for TinkerMatt v0.7.5

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tinkermatt-v074";
+const CACHE = "tinkermatt-v075";
 const CORE = ["./", "./manifest.webmanifest", "./tinkermatt-icon.svg"];
 
 self.addEventListener("install", (event) => {
